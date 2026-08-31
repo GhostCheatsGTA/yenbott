9dd10fc4f8f6b22f54ed811679f9cd114dd73270
GhostCheatsGTA <mystdevelopmentrbx@gmail.com>
2026-08-31 21:46:41 +0100

    Add files via upload

---
+++ utils/database.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+const dbPath = path.join(__dirname, 'database.json');
+
+if (!fs.existsSync(dbPath)) {
+    fs.writeFileSync(dbPath, JSON.stringify({}));
+}
+
+function readData() {
+    try {
+        return JSON.parse(fs.readFileSync(dbPath, 'utf8'));
+    } catch {
+        return {};
+    }
+}
+
+function writeData(data) {
+    fs.writeFileSync(dbPath, JSON.stringify(data, null, 2));
+}
+
+module.exports = {
+    get: async (key) => {
+        const data = readData();
+        return data[key] !== undefined ? data[key] : null;
+    },
+    set: async (key, value) => {
+        const data = readData();
+        data[key] = value;
+        writeData(data);
+        return value;
+    },
+    delete: async (key) => {
+        const data = readData();
+        if (data[key] !== undefined) {
+            delete data[key];
+            writeData(data);
+            return true;
+        }
+        return false;
+    }
+};
